eac86afb360a6080584658a6c35a8ca7a9b8fb63
vijay-thumar <vijaythumar1020@gmail.com>
2023-10-04 10:34:09 +0530

    feat: created file upload

--- server.js
+++ server.js
@@ -1,33 +1,108 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const { promisify } = require("util");
+const Busboy = require("busboy");
+
 const app = express();
+const getFileDetails = promisify(fs.stat);
+
 app.use(express.json());
 app.use(cors());
 
 const uniqueAlphaNumericId = (() => {
-	const heyStack = '0123456789abcdefghijklmnopqrstuvwxyz';
-	const randomInt = () => Math.floor(Math.random() * Math.floor(heyStack.length));
-	
-	return (length = 24) => Array.from({length}, () => heyStack[randomInt()]).join('');
+  const heyStack = "0123456789abcdefghijklmnopqrstuvwxyz";
+  const randomInt = () =>
+    Math.floor(Math.random() * Math.floor(heyStack.length));
+
+  return (length = 24) =>
+    Array.from({ length }, () => heyStack[randomInt()]).join("");
 })();
 
-const getFilePath = (fileName, fileId) => `./uploads/file-${fileId}-${fileName}`;
+const getFilePath = (fileName, fileId) =>
+  `./uploads/file-${fileId}-${fileName}`;
 
-app.post('/upload-request', (req, res) => {
-	if (!req.body || !req.body.fileName) {
-		res.status(400).json({message: 'Missing "fileName"'});
-	} else {
-		const fileId = uniqueAlphaNumericId();
-		fs.createWriteStream(getFilePath(req.body.fileName, fileId), {flags: 'w'});
-		res.status(200).json({fileId});
-	}
+app.post("/upload-request", (req, res) => {
+  if (!req.body || !req.body.fileName) {
+    res.status(400).json({ message: 'Missing "fileName"' });
+  } else {
+    const fileId = uniqueAlphaNumericId();
+    fs.createWriteStream(getFilePath(req.body.fileName, fileId), {
+      flags: "w",
+    });
+    res.status(200).json({ fileId });
+  }
 });
 app.post("/upload-status", (req, res) => {
   res.sendStatus(200);
 });
 app.post("/upload", (req, res) => {
-  res.sendStatus(200);
+  const contentRange = req.headers["content-range"];
+  const fileId = req.headers["x-file-id"];
+  console.log('fileId from x-file-id', fileId)
+  if (!contentRange) {
+    //console.log("Missing Content-Range");
+    return res.status(400).json({ message: 'Missing "Content-Range" header' });
+  }
+
+  if (!fileId) {
+    //console.log("Missing File Id");
+    return res.status(400).json({ message: 'Missing "X-File-Id" header' });
+  }
+
+  const match = contentRange.match(/bytes=(\d+)-(\d+)\/(\d+)/);
+
+  if (!match) {
+    //console.log("Invalid Content-Range Format");
+    return res.status(400).json({ message: 'Invalid "Content-Range" Format' });
+  }
+
+  const rangeStart = Number(match[1]);
+  const rangeEnd = Number(match[2]);
+  const fileSize = Number(match[3]);
+
+  if (rangeStart >= fileSize || rangeStart >= rangeEnd || rangeEnd > fileSize) {
+    return res
+      .status(400)
+      .json({ message: 'Invalid "Content-Range" provided' });
+  }
+
+  const busboy = Busboy({ headers: req.headers });
+
+  busboy.on("error", (e) => {
+    console.error("failed upload", e);
+    res.sendStatus(500);
+  });
+
+  busboy.on("finish", () => {
+    res.sendStatus(200);
+  });
+
+  busboy.on("file", (_, file, fileName) => {
+    console.log('****----**** File Name: ', fileName)
+    const filePath = getFilePath(fileName.filename, fileId);
+    console.log('This is my file path',filePath)
+    getFileDetails(filePath)
+      .then((stats) => {
+        if (stats.size !== rangeStart) {
+          return res.status(400).json({ message: 'Bad "chunk" provided' });
+        }
+
+        file.pipe(fs.createWriteStream(filePath, { flags: "a" }));
+      })
+      .catch((err) => {
+        console.log("Fail to read file", err);
+        return res.status(400).json({
+          message: "no file with provided credentials",
+          credentials: {
+            fileId,
+            fileName,
+          },
+        });
+      });
+  });
+
+  req.pipe(busboy);
 });
 
 app.get("/", (req, res) => {
